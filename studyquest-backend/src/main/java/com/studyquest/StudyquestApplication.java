ef2cfc6a1a3f53698e9219dceeaff1e320125471
dwune08 <dwune158@gmail.com>
2026-08-28 17:32:08 +0900

    feat: 백엔드 및 프론트엔드 프로젝트 통합 저장소 구성

---
+++ studyquest-backend/src/main/java/com/studyquest/StudyquestApplication.java
@@ -0,0 +1,13 @@
+package com.studyquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyquestApplication.class, args);
+	}
+
+}
---
+++ studyquest-backend/src/test/java/com/studyquest/StudyquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
