f23d7fe260cbc898daf58c56052d3e4b905af7c4
Mute <https://github.com/zzzqw59-web>
2026-08-31 14:07:34 +0900

    feat: quizResult MVC 테스트 완료

--- studyquest-backend/src/test/java/com/studyquest/ResultRepository/ResultRepositoryTest.java
+++ studyquest-backend/src/test/java/com/studyquest/ResultRepository/ResultRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.studyquest.ResultRepository;
 
+import com.studyquest.quiz.entity.Quiz;
 import com.studyquest.quizresult.entity.QuizResult;
 import com.studyquest.quizresult.repository.QuizResultRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -17,26 +18,26 @@ public class ResultRepositoryTest {
     @Autowired
     private QuizResultRepository repository;
 
-    @Test
-    void findByStudentNoTest() {
-        List<QuizResult> resultList = repository.findByStudentNo(1L);
-        resultList.forEach(result -> {
-            log.info("result = {}", result);
-        });
-    }
-
-    @Test
-    void JPQLTest() {
-        List<Object[]> list =
-                repository.findStudentResults(1L);
-
-        for (Object[] row : list) {
-
-            QuizResult result = (QuizResult) row[0];
-            Quiz quiz = (Quiz) row[1];
-
-            System.out.println(result.getQuizNo());
-            System.out.println(quiz.getQuizQuestion());
-        }
-    }
+//    @Test
+//    void findByStudentNoTest() {
+//        List<QuizResult> resultList = repository.findStudentResults(1L);
+//        resultList.forEach(result -> {
+//            log.info("result = {}", result);
+//        });
+//    }
+//
+//    @Test
+//    void JPQLTest() {
+//        List<Object[]> list =
+//                repository.findStudentResults(1L);
+//
+//        for (Object[] row : list) {
+//
+//            QuizResult result = (QuizResult) row[0];
+//            Quiz quiz = (Quiz) row[1];
+//
+//            System.out.println(result.getQuizNo());
+//            System.out.println(quiz.getQuizQuestion());
+//        }
+//    }
 }
