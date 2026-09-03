c032e67823d98d765d431467c6728cfbaa94330c
mannschaft3264 <ludwigfrankbauer@gmail.com>
2026-09-03 10:21:39 +0900

    feat: 헤더 배너 수정과 데이터 받아오기

--- studyquest-frontend/study-quest/src/pages/student/StudentMyPage.jsx
+++ studyquest-frontend/study-quest/src/pages/student/StudentMyPage.jsx
@@ -81,6 +81,13 @@ const StudentMyPage = () => {
     statusSpeed: 0,
   };
 
+  const topMenuUserInfo = {
+  userName: currentStatus.studentName,
+  userLevel: currentStatus.statusLevel,
+  currentExp: currentStatus.statusExp,
+  maxExp: currentStatus.nextLevelExp,
+};
+
 const currentLevel = currentStatus.statusLevel ?? 1;
 const profileImage = getStudentLevelImage(currentLevel);
 
@@ -101,7 +108,7 @@ const profileImage = getStudentLevelImage(currentLevel);
   };
 
   return (
-    <BasicLayout>
+    <BasicLayout userInfo={topMenuUserInfo} onLogout={handleLogout}> 
       <div className="w-full max-w-6xl mx-auto px-4 py-2 text-white flex flex-col justify-start">
         {/* 에러/경고 안내 배너 */}
         {warningMessage && (
